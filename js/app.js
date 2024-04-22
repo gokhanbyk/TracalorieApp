0ecf8532862517a865e850c09d00da3743d05668
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-04-22 23:25:55 +0300

    displaying calorie limit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,42 @@
 class CalorieTracker {
   constructor() {
-    this._calorieLimit = 2000;
+    this._calorieLimit = 2200;
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
 
     this._displayCaloriesTotal();
+    this._displayCaloriesLimit();
   }
 
   // public methods/api
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+
+    this._render();
   }
 
   addWorkout(workout) {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+
+    this._render();
   }
 
   // private methods
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById("calories-total");
     totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById("calories-limit");
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
   }
 }
 
@@ -45,7 +59,9 @@ class Workout {
 const tracker = new CalorieTracker();
 
 const breakfast = new Meal("Breakfast", 400);
+const lunch = new Meal("Lunch", 350);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 
 const run = new Workout("Morning Run", 300);
 tracker.addWorkout(run);
